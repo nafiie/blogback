86a4d6ef8c7cbe9ef757c162288de62afb6b3d19
Amusat Gifty <amusatgifty@gmail.com>
2025-10-06 11:06:26 +0100

    api update

--- server.js
+++ server.js
@@ -19,12 +19,7 @@ const app = express();
 connectDb(); 
 
 // Middleware
-app.use(cors({
-  origin: [process.env.CLIENT_URL],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true,
-}));
-
+app.use(cors());
 app.use(express.json()); 
 
 // Routes
